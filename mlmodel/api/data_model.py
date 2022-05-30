d8b519de673bd59d98c4ed83e48b6f48171071af
gokhan <gokhan.koc@gmail.com>
2022-05-30 11:25:22 -0400

    Add live_test

--- mlmodel/api/data_model.py
+++ mlmodel/api/data_model.py
@@ -19,16 +19,16 @@ class BasicInputData(BaseModel):
     workclass: str
     fnlgt: int
     education: str
-    education_num: int = Field(alias='education-num')
-    marital_status: str = Field(alias='marital-status')
+    education_num: int = Field(alias='education_num')
+    marital_status: str = Field(alias='marital_status')
     occupation: str
     relationship: str
     race: str
     sex: str
-    capital_gain: int = Field(alias='capital-gain')
-    capital_loss: int = Field(alias='capital-loss')
-    hours_per_week: int = Field(alias='hours-per-week')
-    native_country: str = Field(alias='native-country')
+    capital_gain: int = Field(alias='capital_gain')
+    capital_loss: int = Field(alias='capital_loss')
+    hours_per_week: int = Field(alias='hours_per_week')
+    native_country: str = Field(alias='native_country')
 
     class Config:
         """
@@ -42,15 +42,15 @@ class BasicInputData(BaseModel):
                     "workclass": "Private",
                     "fnlgt": 77516,
                     "education": "Masters",
-                    "education-num": 13,
-                    "marital-status": "Never-married",
+                    "education_num": 13,
+                    "marital_status": "Never-married",
                     "occupation": "Adm-clerical",
                     "relationship": "Not-in-family",
                     "race": "White",
                     "sex": "Female",
-                    "capital-gain": 2174,
-                    "capital-loss": 0,
-                    "hours-per-week": 40,
-                    "native-country": "United-States",
+                    "capital_gain": 2174,
+                    "capital_loss": 0,
+                    "hours_per_week": 40,
+                    "native_country": "United-States",
             }
         }
---
+++ mlmodel/api/live_test_api.py
@@ -0,0 +1,30 @@
+"""
+Example script for querying the live API hosted on
+Heroku.
+"""
+import os
+import requests
+
+URL = "https://udacity3_project.herokuapp.com"
+# URL = "http://127.0.0.1:8000"
+
+
+response = requests.post(os.path.join(URL, "predictions"), json={
+                    "age": 55,
+                    "workclass": "Private",
+                    "fnlgt": 77516,
+                    "education": "Masters",
+                    "education_num": 16,
+                    "marital_status": "Never-married",
+                    "occupation": "Adm-clerical",
+                    "relationship": "Not-in-family",
+                    "race": "White",
+                    "sex": "Female",
+                    "capital_gain": 10000,
+                    "capital_loss": 400,
+                    "hours_per_week": 45,
+                    "native_country": "United-States",
+            })
+
+print(response.status_code)
+print(response.json())
